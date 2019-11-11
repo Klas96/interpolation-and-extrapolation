2789f049cc2192ed784664daa3885ba57c8757d2
klas96 <klas0holmgren@gmail.com>
2019-11-11 16:38:36 +0100

    main

---
+++ main.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+
+#Creating a Series by passing a list of values, letting pandas create a default integer index:
+
+s = pd.Series([1, 3, 5, np.nan, 6, 8])
+
+#Creating a DataFrame by passing a NumPy array, with a datetime index and labeled columns:
+
+dates = pd.date_range('20130101', periods=6)
+
+#Creating a DataFrame by passing a dict of objects that can be converted to series-like.
+
+df2 = pd.DataFrame({'A': 1.,
+                    'B': pd.Timestamp('20130102'),
+                    'C': pd.Series(1, index=list(range(4)), dtype='float32'),
+                    'D': np.array([3] * 4, dtype='int32'),
+                    'E': pd.Categorical(["test", "train", "test", "train"]),
+                    'F': 'foo'})
+
+
+#The columns of the resulting DataFrame have different dtypes.
+
+df2.dtypes
